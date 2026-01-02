4a3b2033c1d8683dd221182bc0f45f3e4ac69dc4
Justice Okpara <Justiceokpara7@gmail.com>
2026-01-01 20:31:13 -0500

    feat: add VPC module (public/private subnets, single NAT, EKS-ready tags)

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,21 @@
+variable "project_name" {
+  description = "Project name used for naming and tagging."
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "az_count" {
+  description = "Number of availability zones to use."
+  type        = number
+  default     = 2
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name used for subnet tagging."
+  type        = string
+}
