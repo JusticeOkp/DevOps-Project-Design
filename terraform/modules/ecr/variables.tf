3ed6b697ec736e4dd0a88d80148d0472eece9c77
Justice Okpara <Justiceokpara7@gmail.com>
2026-01-05 18:33:33 -0500

    feat: add ECR module with lifecycle policy

---
+++ terraform/modules/ecr/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  description = "Project name used for tagging."
+  type        = string
+}
+
+variable "repo_name" {
+  description = "ECR repository name."
+  type        = string
+}
